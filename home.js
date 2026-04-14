0fea0842a31592e9245fac916cad018f7a95695d
Rawan Hatem Hussein <167790256+RawanHatem77@users.noreply.github.com>
2026-04-15 01:27:15 +0200

    Update home.js

--- home.js
+++ home.js
@@ -8,43 +8,36 @@ async function updateModel() {
     }
 
     try {
-       const response = await fetch(`https://smartsizeecommerce.runasp.net/api/SizeCalculator/calculate?height=${height}&weight=${weight}`);
+        const response = await fetch(
+            `https://smartsizeecommerce.runasp.net/api/SizeCalculator/calculate?height=${height}&weight=${weight}`
+        );
+
+        const avatar = document.getElementById("avatar");
 
         if (response.ok) {
             const data = await response.json();
-            
+
             document.getElementById("sizeResult").innerText = data.sizeName;
-            const avatar = document.getElementById("avatar");
+
             avatar.src = data.modelImageUrl;
 
-            if (data.sizeName === "2XL") {
-                avatar.style.transform = "scale(1.15)"; 
-                avatar.style.transition = "0.5s";       
-            } else if (data.sizeName === "XL") {
-                avatar.style.transform = "scale(1.08)"; 
-                avatar.style.transition = "0.5s";
-            } else if (data.sizeName === "S") {
-                avatar.style.transform = "scale(0.85)"; 
-                avatar.style.transition = "0.5s";
-            } else {
-                avatar.style.transform = "scale(1)";  
-                avatar.style.transition = "0.5s";
-            }
-        } 
-        else if (response.status === 404) {
-        document.getElementById("sizeResult").innerText = "Out of Range";
-        
-        const avatar = document.getElementById("avatar");
-        avatar.src = "model.png"; 
-        avatar.style.transform = "scale(1)";
+            let scale = 1;
 
-        alert("عذراً، المقاس المطلوب Out of Range (خارج النطاق المدعوم حالياً).");
-    }
-        else {
-            alert("حدث خطأ غير متوقع في السيرفر.");
+            if (data.sizeName === "2XL") scale = 1.15;
+            else if (data.sizeName === "XL") scale = 1.08;
+            else if (data.sizeName === "S") scale = 0.85;
+
+            avatar.style.transform = `scale(${scale})`;
+
+        } else {
+            document.getElementById("sizeResult").innerText = "Out of Range";
+
+            avatar.src = "model.png";
+            avatar.style.transform = "scale(1)";
         }
+
     } catch (error) {
-        console.error("Error:", error);
-        alert("تأكدي من تشغيل الـ API في Visual Studio أولاً.");
+        console.error(error);
+        alert("Error connecting to server");
     }
 }
